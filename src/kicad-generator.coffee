qeda/qeda

commit faa3aa77789c3ba9826e8c3c3be05aee070310fc

Allow for new and old style pin name negation (#108)
With this change, both KiCad old (`~pin~`/`~pin`) and new style (`~{pin}`) pin name negation are understood by QEDA and correctly converted.

src/kicad-generator.coffee
@@ -149,6 +149,15 @@ class KicadGenerator
     fs.writeSync fd, ")\n" # module
 
   #
+  # Format pin names for KiCad
+  #
+  _formatPinName: (name) ->
+    formatted = name.replace /~{([^}]+)}/g, '~$1~'
+    if (formatted.match(/~/g) || []).length % 2 == 1
+      formatted += '~'
+    return formatted
+
+  #
   # Write symbol entry to library file
   #
   _generateSymbol: (fd, element) ->
@@ -199,7 +208,7 @@ class KicadGenerator
       for shape in symbol.shapes
         symObj = @_symbolObj shape
         switch symObj.kind
-          when 'pin' then fs.writeSync fd, "X #{symObj.name} #{symObj.number} #{symObj.x} #{symObj.y} #{symObj.length} #{symObj.orientation} #{symObj.fontSize} #{symObj.fontSize} #{i} 1 #{symObj.type}#{symObj.shape}\n"
+          when 'pin' then fs.writeSync fd, "X #{@_formatPinName(symObj.name)} #{symObj.number} #{symObj.x} #{symObj.y} #{symObj.length} #{symObj.orientation} #{symObj.fontSize} #{symObj.fontSize} #{i} 1 #{symObj.type}#{symObj.shape}\n"
           when 'rectangle' then fs.writeSync fd, "S #{symObj.x1} #{symObj.y1} #{symObj.x2} #{symObj.y2} #{i} 1 #{symObj.lineWidth} #{symObj.fillStyle}\n"
           when 'line' then fs.writeSync fd, "P 2 #{i} 1 #{symObj.lineWidth} #{symObj.x1} #{symObj.y1} #{symObj.x2} #{symObj.y2} N\n"
           when 'circle' then fs.writeSync fd, "C #{symObj.x} #{symObj.y} #{symObj.radius} #{i} 1 #{symObj.lineWidth} #{symObj.fillStyle}\n"

src/kicad6-generator.coffee
@@ -114,6 +114,14 @@ class Kicad6Generator
     fs.writeSync fd, ")\n" # module
 
   #
+  # Format pin names for KiCad 6
+  #
+  _formatPinName: (name) ->
+    formatted = name.replace /~([^~{}]+)~/g, '~{$1}'
+    formatted = formatted.replace /~([^~{}]+)/g, '~{$1}'
+    return formatted
+
+  #
   # Write symbol file
   #
   _generateSymbol: (fd, element) ->
@@ -261,7 +269,7 @@ class Kicad6Generator
             fs.writeSync fd, "\n"
             fs.writeSync fd, sprintf("      (at #{@f} #{@f} #{symObj.orientation})\n", symObj.x, symObj.y)
             fs.writeSync fd, sprintf("      (length #{@f})\n", symObj.length)
-            fs.writeSync fd, "      (name \"#{symObj.name}\"\n"
+            fs.writeSync fd, "      (name \"#{@_formatPinName(symObj.name)}\"\n"
             fs.writeSync fd, "        (effects (font (size #{symObj.fontSize} #{symObj.fontSize})"
             if symObj.bold then fs.writeSync fd, " bold"
             if symObj.italic then fs.writeSync fd, " italic"

src/kicad7-generator.coffee
@@ -140,6 +140,14 @@ class Kicad7Generator
     fs.writeSync fd, ")\n" # module
 
   #
+  # Format pin names for KiCad 7
+  #
+  _formatPinName: (name) ->
+    formatted = name.replace /~([^~{}]+)~/g, '~{$1}'
+    formatted = formatted.replace /~([^~{}]+)/g, '~{$1}'
+    return formatted
+
+  #
   # Write symbol file
   #
   _generateSymbol: (fd, element) ->
@@ -310,7 +318,7 @@ class Kicad7Generator
               fs.writeSync fd, "line"
             fs.writeSync fd, sprintf(" (at #{@f} #{@f} #{symObj.orientation})", symObj.x, symObj.y)
             fs.writeSync fd, sprintf(" (length #{@f})\n", symObj.length)
-            fs.writeSync fd, "        (name \"#{symObj.name}\" (effects (font (size #{symObj.fontSize} #{symObj.fontSize})"
+            fs.writeSync fd, "        (name \"#{@_formatPinName(symObj.name)}\" (effects (font (size #{symObj.fontSize} #{symObj.fontSize})"
             if symObj.bold then fs.writeSync fd, " bold"
             if symObj.italic then fs.writeSync fd, " italic"
             fs.writeSync fd, ")"

src/geda-generator.coffee
@@ -139,8 +139,11 @@ class GedaGenerator
                 angle = 90
                 alignment = 6
             name = shape.name.toString()
-            if name.includes('~') or shape.inverted
-              name = name.replace(/^~/,'').replace(/~$/,'')
+            if name.includes('~')
+              name = name.replace /~{([^}]+)}/g, '\\_$1\\_'
+              name = name.replace /~([^~]+)~/g, '\\_$1\\_'
+              name = name.replace /~([^~]+)/g, '\\_$1\\_'
+            if shape.inverted
               name = "\\_#{name}\\_"
             fs.writeSync fd, "T #{xt} #{yt} 9 8 #{if element.schematic.showPinNames then 1 else 0} 1 #{angle} #{alignment} 1\n"
             fs.writeSync fd, "pinlabel=#{name}\n"

commit b00333b02ed0987ac7c3f5ba992b919444e99ef5

Fix handling of numeric pin names
Commit faa3aa7 broke numeric pin names in all KiCad versions

src/kicad-generator.coffee
@@ -152,7 +152,7 @@ class KicadGenerator
   # Format pin names for KiCad
   #
   _formatPinName: (name) ->
-    formatted = name.replace /~{([^}]+)}/g, '~$1~'
+    formatted = String(name).replace /~{([^}]+)}/g, '~$1~'
     if (formatted.match(/~/g) || []).length % 2 == 1
       formatted += '~'
     return formatted

src/kicad6-generator.coffee
@@ -117,7 +117,7 @@ class Kicad6Generator
   # Format pin names for KiCad 6
   #
   _formatPinName: (name) ->
-    formatted = name.replace /~([^~{}]+)~/g, '~{$1}'
+    formatted = String(name).replace /~([^~{}]+)~/g, '~{$1}'
     formatted = formatted.replace /~([^~{}]+)/g, '~{$1}'
     return formatted
 

src/kicad7-generator.coffee
@@ -143,7 +143,7 @@ class Kicad7Generator
   # Format pin names for KiCad 7
   #
   _formatPinName: (name) ->
-    formatted = name.replace /~([^~{}]+)~/g, '~{$1}'
+    formatted = String(name).replace /~([^~{}]+)~/g, '~{$1}'
     formatted = formatted.replace /~([^~{}]+)/g, '~{$1}'
     return formatted
 